f2f304f5105192a4c894b24ffdb47bca7fe0ad6c
Takuya Ikeshima <ikeshi1222@gmail.com>
2023-02-28 14:14:43 +0000

    mypage画面表示1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Admin\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(KeepController::class)->group(function(){
+    Route::get('mypage','home');
 });
