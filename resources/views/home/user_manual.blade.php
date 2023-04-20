73e8137f908d185f8ba3bf1e0487a69352082141
Takuya Ikeshima <ikeshi1222@gmail.com>
2023-04-20 10:28:57 +0000

    全体のレイアウトは完成

--- resources/views/home/user_manual.blade.php
+++ resources/views/home/user_manual.blade.php
@@ -3,10 +3,20 @@
 @section('title', 'ご利用ガイド')
 
 @section('content')
-    <div>
-        <h1>画面：ご利用ガイド</h1>
-        <a>この画面はご利用ガイドです。</a>
-        <br>
+    <div class="start-guide">
+        <h2>ご利用ガイド</h2>
+        <div class="guide-text">
+            <ul>
+                <li>あ</li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        
         <!-- CSSでボタン表示 -->
         <a href="{{ route('home.mypage') }}">マイページ</a>
     </div>
